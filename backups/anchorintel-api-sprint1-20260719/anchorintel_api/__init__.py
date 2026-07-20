4892c60986b5456f43aad0193032cfb280875a30
Ricky Jarnagin <dsynegrafix@gmail.com>
2026-07-19 22:04:06 -0500

    Remove local Sprint 1 backup from repository

--- backups/anchorintel-api-sprint1-20260719/anchorintel_api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""AnchorIntel API: S.P.A.T.I.A.L. lifecycle services for AnchorOS."""
-
-from .app import AnchorIntelApplication
-from .service import AnchorIntelService
-
-__all__ = ["AnchorIntelApplication", "AnchorIntelService"]
-__version__ = "0.1.0"
-
